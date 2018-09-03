a2d75c30da9c0fa7359fe1315b118f6625ccf393
Jack Bowen <jackbowen92@gmail.com>
2018-09-03 01:20:08 -0700

    Update install script and README

---
+++ README.md
@@ -0,0 +1,20 @@
+# FreshBytes Backend
+
+The backend to run FreshBytes, a hydroponic and aquaponic monitoring and control system. The system runs on [this](https://github.com/jackbowen/FreshBytes-Hardware) and is interacted with through [this](https://github.com/jackbowen/FreshBytes-WebApp) app.
+
+## Instructions
+
+### Install
+
+Navigate to the folder containing your download and run:
+
+```
+sudo ./install.sh
+```
+
+### Running the software
+
+```
+node freshServer.js
+```
+
--- install.sh
+++ install.sh
@@ -1,6 +1,37 @@
 #!/bin/sh
-# NOT WORKING YET. JUST A PLACEHOLDER
-LIST_OF_APPS="node mysql"
 
-aptitude update
-aptitude install -y $LIST_OF_APPS
+#apt-get update
+
+echo "Checking for dependencies"
+#install nodejs 
+#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
+hash node 2>/dev/null || {
+	echo "-> Installing node.";
+       	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -;
+	sudo apt-get install -y nodejs;
+}
+
+hash npm 2>/dev/null || {
+	echo "-> Installing npm.";
+	sudo apt-get install npm;
+}
+
+hash mysql 2>/dev/null || {
+	echo "-> Installing mysql.";
+	sudo apt-get install mysql-server;
+}
+
+#TODO: make the grep not print to the command line
+grep dtoverlay=w1-gpio /boot/config.txt 2>/dev/null || {
+	echo "-> Enabling one-wire interface.";
+	echo "# Enable the one-wire interface" >> /boot/config.txt;
+	echo "dtoverlay=w1-gpio" >> /boot/config.txt;
+}
+
+# TODO: make this in an if block
+echo 17 > /sys/class/gpio/export
+echo 27 > /sys/class/gpio/export
+
+echo "Done."
